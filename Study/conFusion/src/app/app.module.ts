a12aacd0ac2e01ec76624a4aaca27d4df59d8422
lubarog13 <lubarog13@gmail.com>
2021-10-09 10:53:11 +0300

    angular неделя 2 промежуток

--- Study/conFusion/src/app/app.module.ts
+++ Study/conFusion/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
@@ -14,12 +14,23 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +43,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatListModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Study/conFusion/src/app/menu/menu.component.ts
+++ Study/conFusion/src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-menu',
@@ -8,12 +8,14 @@ import { DISHES } from '../shared/dishes';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-  dishes: Dish[] = DISHES;
-  selectedDish: Dish;
-  
-  constructor() { }
+  dishes: Dish[];
 
-  ngOnInit(): void {
+  selectedDish: Dish;
+
+  constructor(private dishService: DishService) { }
+  
+  ngOnInit() {
+    this.dishes = this.dishService.getDishes();
   }
   onSelect(dish: Dish){
     this.selectedDish = dish
