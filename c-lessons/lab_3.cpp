5e9e5a66819cf233c4ea52d9fb93caa8b06780f8
Lyubov Rogozina <lubarog13@gmail.com>
2022-11-13 22:08:22 +0300

    Исправлена 3 лабораторная, добавлена погрешность

--- c-lessons/lab_3.cpp
+++ c-lessons/lab_3.cpp
@@ -2,27 +2,35 @@
 #include <math.h>
 
 int main() {
-    double x, y, r, a, b;
+    double x, y, r, a, b, eps;
     std::cout<<"Введите x: ";
     std::cin>>x;
     std::cout<<"Введите y: ";
     std::cin>>y;
     std::cout<<"Введите радиус окружности: ";
     std::cin>>r;
+    if(r<=0) {
+        std::cout<<"Введите положительный радиус\n";
+        return 0;
+    }
     std::cout<<"Введите коэффицент a: ";
     std::cin>>a;
     std::cout<<"Введите коэффицент b: ";
     std::cin>>b;
-    if(x > 0) {
-        std::cout<<"Точка не входит в область";
-        return 0;
-    } else if (a*x + b > y ) {
-        std::cout<<"Точка не входит в область";
-        return 0;
-    } else if (sqrt(pow(x, 2) + pow(y, 2)) >= r) {
-        std::cout<<"Точка не входит в область";
-        return 0;
+    std::cout<<"Введите погрешность: ";
+    std::cin>>eps;
+    if(eps<=0 || eps>=1) {
+        std::cout<<"Неверно введена погрешность, требуется от 0 до 1\n";
     }
-    std::cout<<"Точка входит в область\n";
+    else if (y > 0 + eps) {
+        std::cout<<"Точка не входит в область\n";
+    } else if (a*x + b < y - eps ) {
+        std::cout<<"Точка не входит в область\n";
+    } else if (sqrt(pow(x, 2) + pow(y, 2)) > r+eps) {
+        std::cout<<"Точка не входит в область\n";
+    }
+    else {
+        std::cout<<"Точка входит в область\n";
+    }
     return 0;
 }
