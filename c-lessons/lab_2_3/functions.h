18d9dddf29ce549f27347365c943b63cbc9db4e1
lubarog13 <lubarog13@gmail.com>
2023-05-26 00:26:32 +0300

    Исправлена лабораторная 3

--- c-lessons/lab_2_3/functions.h
+++ c-lessons/lab_2_3/functions.h
@@ -1,6 +1,8 @@
 #include "header.h"
 
-string inputString(string message);
-void addSymbols(string &input, string substring, char symbol, int count);
-int inputInt(string message);
+int inputInt(string message, int min);
 char inputChar(string message);
+string inputString(string message);
+
+void addSymbols(string &input, string substring, char symbol, int count);
+void outputString(string input);
