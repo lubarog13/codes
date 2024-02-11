f5fd9f64419491e8ab1b63e19d3384ae938b9b85
lubarog13 <lubarog13@gmail.com>
2024-02-11 17:02:54 +0300

    Лаба 1 по ит

--- c-lessons/lab_4_1/main.cpp
+++ c-lessons/lab_4_1/main.cpp
@@ -1,13 +1,37 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <limits>
+
+int inputInt(std::string message, int min, int max)
+{
+    int n;
+    std::cout << message;
+    while ((!(std::cin >> n)) || (n < min) || (n > max))
+    {
+        std::cout << "Ошибка! Неправильно введено число. Оно должно быть больше " << min << " и меньше " << max << "." << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << message;
+    }
+    // cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    return n;
+}
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-        sf::Vertex line[] =
-    {
-        sf::Vertex(sf::Vector2f(10.f, 10.f)),
-        sf::Vertex(sf::Vector2f(150.f, 150.f))
-    };
+    int startX, startY, endX, endY;
+    startX = inputInt("Введите координату X первой точки линии: ", 0, 640);
+    startY = inputInt("Введите координату Y первой точки линии: ", 0, 480);
+    endX = inputInt("Введите координату X последней точки линии: ", 0, 640);
+    endY = inputInt("Введите координату Y последней точки линии: ", 0, 480);
+    sf::Vertex startDot = sf::Vector2f(startX, startY);
+    sf::Vertex endDot = sf::Vector2f(endX, endY);
+    startDot.color = sf::Color::Red;
+    endDot.color= sf::Color::White;
+    sf::Vertex line[] = {startDot, endDot};
+
+    sf::RenderWindow window(sf::VideoMode(640, 480), L"Простые фигуры");
 
     while (window.isOpen())
     {
@@ -22,6 +46,9 @@ int main()
          window.draw(line, 2, sf::Lines);
         window.display();
     }
+
+    std::cout << "Введите любую клавишу для завершения программы ... "<<std::flush;
+    std::cin.ignore( std::numeric_limits <std::streamsize> ::max(), '\n' );
 
     return 0;
 }
